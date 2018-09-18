c3f1b4d7f2e1ef1a1a97b4f4c756e9779a39628d
evgeniy-bizhanov <evgeniy.bizhanov@me.com>
2018-09-18 22:49:38 +0500

    init

---
+++ Avia/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Avia
+//
+//  Created by Евгений Бижанов on 18.09.2018.
+//  Copyright © 2018 Евгений Бижанов. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Avia/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Avia
+//
+//  Created by Евгений Бижанов on 18.09.2018.
+//  Copyright © 2018 Евгений Бижанов. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
